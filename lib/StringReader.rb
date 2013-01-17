03822202ee02820afb0eb692fca7450429a582b6
Chris <chris@chris-ThinkPad-T510.(none)>
2013-01-17 13:55:28 -0700

    Initial Files

---
+++ lib/StringReader.rb
@@ -0,0 +1,49 @@
+class StringReader
+  private
+  
+  @@readFromPrompt = "Read from? \n1. File\n2. Console"
+  @@choiceInt = 1
+  @@fileNamePrompt = "What is the name of the file?"
+  @@readFromConsoleInstructions = "To input the strings manually, simply type each string followed by return. When you are finished, type 'done' then press return to halt execution..."
+  @@endConsoleReadWord = "done"
+  
+  public
+  
+  def initialize
+    @hands = Array.new
+  end
+  
+  def readStrings
+    puts @@readFromPrompt
+    choice = gets.chomp.to_i
+    retStrings = ""
+    retStrings = readFromFile if choice == @@choiceInt
+    retStrings = readFromConsole if choice == (@@choiceInt + 1)
+    return retStrings
+  end
+  
+  private
+  
+  def readFromFile
+    puts @@fileNamePrompt
+    fileName = gets.chomp
+    allString = Array.new
+    tempFile = File.open(fileName).each_line { |s|
+        allHands << s
+    }
+    return allHands
+  end
+   
+  def readFromConsole
+     puts @@readFromConsoleInstructions
+     allString = Array.new
+     input = gets.chomp
+     while(input != @@endConsoleReadWord)
+       allString << input
+       input = gets.chomp
+     end
+     return allString
+  end
+  
+ 
+end
