ef7c0b490855bc673d55a31b6c1759e85a585754
chague <chague@student.neumont.edu>
2013-01-18 15:46:33 -0700

    changed to csv files, also prints independant analysis to console

--- lib/AnalysisWriter.rb
+++ lib/AnalysisWriter.rb
@@ -1,29 +1,71 @@
 require "csv"
+require "pry"
 
 class AnalysisWriter
 
   attr_reader :strings
 
-  @@FILE = "../bin/analysis.txt"
-  @@FILE_OPTION_WRITE = "w"
+  @@FILE = "ValidStrings.log"
+  @@FILE_FAIL = "InvalidStrings.log"
+  @@FILE_OPTION_WRITE = "a"
+  @@SPLIT_CHAR = ", "
+  @@DEFAULT_OPTIONS = { :col_sep => ",",
+                    :row_sep => :auto,
+                    :quote_char => '"',
+                    :converters => nil,
+                    :unconverted_fields => nil,
+                    :headers => @headers,
+                    :return_headers => false,
+                    :header_converters => nil,
+                    :skip_blanks => false,
+                    :force_quotes => false }
   
-  def initialize(strings)
+  
+  
+  def initialize(strings, dir)
+    @directory = dir
+    Dir::mkdir(@directory)
     @strings = strings
+    @file = dir + @@FILE
+    @file_fail = dir + @@FILE_FAIL
+    @headers = ["String Size", "Max String Size", "String", "Error Message"]
   end
 
   def writeFile
-    begin
-      CSV.open(@@FILE, @@FILE_OPTION_WRITE) do |f|
+
+    unless (File.exists?(@file))
+      CSV.open(@file, @@FILE_OPTION_WRITE) do |f|
+        f << @headers
+      end 
+      
+    end
+    
+    CSV.open(@file, @@FILE_OPTION_WRITE) do |f|
         for line in @strings
-          for sing in line
-            f << sing
+          inputs = line.split(@@SPLIT_CHAR)
+          if(inputs[3] == nil)
+            f << inputs
+          else
+            writeFail(inputs)
           end
         end
-      end
-    rescue
-      CSV.new(@@FILE, @@FILE_OPTION_WRITE)
-      retry
     end
+
   end
+  
+  def writeFail(strings)
+    unless (File.file?(@file_fail))
+      CSV.open(@file_fail, @@FILE_OPTION_WRITE) do |f|
+        f << @headers
+      end
+    end
+    
+      CSV.open(@file_fail, @@FILE_OPTION_WRITE) do |f|
+        f << strings
+      end
+   
+  end
+  
+  
 
 end
